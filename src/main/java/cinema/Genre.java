18f369bc75c679439fcb8f3caf3bb8706de30b09
= <=>
2026-05-29 17:38:34 +0200

    Created Genre class for Movie class plus inserted bin folder that contains .class files.

---
+++ src/main/java/cinema/Genre.java
@@ -0,0 +1,11 @@
+package cinema;
+
+public enum Genre{
+    ACTION,
+    COMEDY,
+    DRAMA,
+    HORROR,
+    SCI_FI,
+    ROMANCE,
+    ANIMATION
+}
---
+++ src/main/java/cinema/Movie.java
@@ -0,0 +1,42 @@
+package cinema;
+import java.time.Duration;
+
+public class Movie{
+    String title;
+    Duration duration;  
+    Genre genre;
+
+    public Movie(String title, Duration duration, Genre genre){
+        this.title = title;
+        this.duration = duration;
+        this.genre = genre;
+    }
+
+    public Genre getGenre(){
+        return this.genre;
+    }
+    public void setGenre(Genre genre){
+        this.genre = genre;
+    }
+
+    public Duration getDuration(){
+        return this.duration;
+    }
+    public void setDuration(Duration duration){
+        this.duration = duration;
+    }
+    
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    @Override
+    public String toString(){
+        return "Film: " + getTitle() + 
+                "Genere: " + getGenre() + 
+                "Durata: " + getDuration().toMinutes() + " minuti.";
+    }
+}
