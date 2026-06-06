f527059faee8e1c9346b0d70823aa229d74b9bef
= <=>
2026-06-06 15:46:09 +0200

    Create films in main. Implementing seat class

--- src/main/java/cinema/Main.java
+++ src/main/java/cinema/Main.java
@@ -1,7 +1,14 @@
 package cinema;
+import java.time.Duration;
 
 public class Main{
     public static void main(String[] args){
         
+        // Generate movies
+        Movie film1 = new Movie("Scary Movie", Duration.ofMinutes(94), Genre.COMEDY);
+        Movie film2 = new Movie("Toy Story 5", Duration.ofMinutes(102), Genre.ANIMATION);
+        Movie film3 = new Movie("Supergirl", Duration.ofMinutes(110), Genre.ACTION);
+
+
     }
 }
---
+++ src/main/java/cinema/Seat.java
@@ -0,0 +1,15 @@
+package cinema;
+
+public class Seat{
+    String seatId;
+    String row;
+    String column;
+
+    public Seat(String seatId, String row, String column){
+        this.seatId = seatId;
+        this.row = row;
+        this.column = column;
+
+    }
+
+}
