3cc5c43b7ed3af7e8b886c1ef46fab71e17bcdb6
= <=>
2026-06-08 19:42:33 +0200

    Added new functions to catalog class.

--- src/main/java/cinema/Catalog.java
+++ src/main/java/cinema/Catalog.java
@@ -14,6 +14,19 @@ public class Catalog{
         this.movies.remove(movie);
     }
 
+    public ArrayList<Movie> filterByTitle(String movieName){
+        ArrayList<Movie> filteredMovies = new ArrayList<>();
+        for(Movie m: movies){
+            if(m.getTitle().toLowerCase().contains(movieName.toLowerCase()))
+                filteredMovies.add(m);
+        }
+        return filteredMovies;
+    }
+
+    public Movie getMovieByIndex(int index){
+        return movies.get(index-1);
+    }
+
     public String toString(){
         if(movies.size()==0)
             return "Il catalogo è vuoto";  
